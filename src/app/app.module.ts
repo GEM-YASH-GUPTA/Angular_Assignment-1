9c9acadf027c64d583d4b5d8cf5140e49f3bed5f
Yash-Gemini <Yash.A.Gupta@Geminisolutions.com>
2022-05-09 20:50:13 +0530

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { TemplateDrivenComponent } from './template-driven/template-driven.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {FormsModule} from '@angular/forms'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateDrivenComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/template-driven/template-driven.component.ts
@@ -0,0 +1,75 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-template-driven',
+  templateUrl: './template-driven.component.html',
+  styleUrls: ['./template-driven.component.css']
+})
+export class TemplateDrivenComponent implements OnInit {
+
+  
+  constructor() { }
+  formData = []; 
+  // @ViewChild('userForm') Forms: NgForm;
+  ngOnInit(): void {
+  }
+  onSubmit(data : any){
+    console.warn(data);
+  }
+  user = {
+    username: "",
+    college: "",
+    cgpa: "",
+    year: "",
+    doj:"",
+    skills: "",
+    file :""
+  }
+  array:any=[];
+  // objectKeys=Object.keys;
+  submitted = false;
+  submitdata(data:any){
+      if(data.valid){
+        // console.log("yash");
+        // this.formData.push(this.userForm.value)
+        // var array=[data];
+        this.submitted=true;
+        // this.array.push(data.value)
+        // console.log(this.array)
+
+        let values = data.value;
+        
+
+        console.log(this.array);
+        let obj: any = {
+          
+          username: values.username,
+          college:values.college,
+          cgpa: values.cgpa,
+          year:values.year,
+          doj:values.doj,
+          skills: values.skills,
+          file :values.file
+        };
+      //  let  use = {
+      //     username: "",
+      //     college: "",
+      //     cgpa: "",
+      //     year: "",
+      //     doj:"",
+      //     skills: "",
+      //     file :""
+      //   }
+    
+        this.array.push(Object.values(obj));
+        // data.reset();
+        console.log(this.array)
+      }
+      else{
+        console.log("invalid");
+      }
+  }
+
+
+}
